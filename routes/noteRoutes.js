15db2de1e1b201683437bdbda1f0e1355774b213
Praveshvermaa <praveshdhakd62@gmail.com>
2025-06-26 01:10:26 +0530

    notes delete

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../utils/cloudinary'); // Cloudinary upload configuration
-const { uploadNote, uploadPaper } = require('../controllers/noteController'); // Import controller
-const authMiddleware = require('../middleware/authMiddleware'); 
-const Note = require('../models/note') ;
+const { uploadNote, uploadPaper } = require('../controllers/noteController'); 
+const authMiddleware = require('../middleware/authMiddleware');
+const Note = require('../models/note');
 const paper = require('../models/paper');
+const User = require('../models/user');
+const { v2: cloudinary } = require("cloudinary");
 
 
 // Define the route for uploading notes
@@ -15,17 +17,56 @@ router.post('/uploadpaper', authMiddleware, upload.single('file'), uploadPaper);
 
 // fetching of notes by uploader
 router.get('/fetch-all', authMiddleware, async (req, res) => {
-    try {
-      const uploaderId = req.user._id; 
-      const notes = await Note.find({ uploader: uploaderId });
-       // Fetch only the notes uploaded by the current user
-      res.status(200).json({success:true,notes});  // Send the notes to the frontend
-    } catch (error) {
-      console.error("Error fetching notes:", error);
-      res.status(500).json({ message: "Error fetching notes" });
+  try {
+    const uploaderId = req.user._id;
+    const notes = await Note.find({ uploader: uploaderId });
+    // Fetch only the notes uploaded by the current user
+    res.status(200).json({ success: true, notes });  // Send the notes to the frontend
+  } catch (error) {
+    console.error("Error fetching notes:", error);
+    res.status(500).json({ message: "Error fetching notes" });
+  }
+});
+// DELETE /notes/delete/:id
+router.delete("/delete/:id", authMiddleware, async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const note = await Note.findById(noteId);
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
     }
-  });
-  
+// function to extract id of note from cloudinary
+    function extractPublicId(url) {
+      const parts = url.split("/");
+      const filenameWithExt = parts[parts.length - 1];
+      const publicId = filenameWithExt.split(".")[0];
+      return publicId;
+    }
+    if (note.fileUrl) {
+      const publicId = extractPublicId(note.fileUrl);
+      await cloudinary.uploader.destroy(publicId);
+      console.log("delete from cloudinary");
+      
+    }
+
+    // Remove noteId from the uploader's user.notes array
+    if (note.uploader) {
+      await User.findByIdAndUpdate(req.user._id, {
+        $pull: { notesIds: note._id }
+      });
+    }
+
+    // Delete note from the database
+    await Note.findByIdAndDelete(noteId);
+
+    res.status(200).json({ message: "Note deleted successfully" });
+  } catch (error) {
+    console.error("Delete Error:", error);
+    res.status(500).json({ message: "Failed to delete note", error });
+  }
+});
+
+
 
 // POST /api/notes - Get Notes based on Average Rating
 router.post("/", async (req, res) => {
@@ -87,7 +128,7 @@ router.post('/rate', async (req, res) => {
     const avgRating = note.ratingSum / note.totalRatingUsers;
 
     res.status(200).json({
-      success:true,
+      success: true,
       message: 'Rating added successfully!',
       averageRating: avgRating.toFixed(1),
     });
